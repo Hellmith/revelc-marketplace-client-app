13e0d3e776d62f8b06fc61094f82be8dc4d33dcf
Kirill Borisov <neckpuntop@gmail.com>
2023-08-17 21:55:00 +0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+'use client'
+
+import { Button } from 'primereact/button'
+
 export default function Home() {
-  return (
-    <p>Hello world!</p>
-  )
+    return (
+        <>
+            <div>
+                <p className='text-3xl font-bold'>Hello world!</p>
+                <Button label='Кнопка' />
+            </div>
+        </>
+    )
 }
